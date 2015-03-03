ca576a1763910edd10107fbea53bfd50f216f0f9
jackdcrawford <jackcrawford19@gmail.com>
2015-03-03 15:07:22 +0000

    rebuild pages

--- _plugins/to_gravatar.rb
+++ _plugins/to_gravatar.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module Jekyll
   module GravatarFilter
     def to_gravatar(input, size=400)
-      "//www.gravatar.com/avatar/#{hash(input)}?s=#{size}"
+      "http://www.gravatar.com/avatar/#{hash(input)}?s=#{size}"
     end
 
     private :hash
